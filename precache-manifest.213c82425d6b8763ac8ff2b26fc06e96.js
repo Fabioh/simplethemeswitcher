9f4e00eb6be44fecc63cbd96cd805111fe1752be
Fábio Henrique Gabriele <fabiohenriqueg@gmail.com>
2020-03-22 01:34:41 -0300

    Updates

---
+++ precache-manifest.213c82425d6b8763ac8ff2b26fc06e96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d16c66c363c256c54ea0e23a060b04c",
+    "url": "/simplethemeswitcher/index.html"
+  },
+  {
+    "revision": "05ba18decb5e63986160",
+    "url": "/simplethemeswitcher/static/js/2.e47a151a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simplethemeswitcher/static/js/2.e47a151a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b755de67c061247579fc",
+    "url": "/simplethemeswitcher/static/js/main.ef050838.chunk.js"
+  },
+  {
+    "revision": "168c87733937a64179e0",
+    "url": "/simplethemeswitcher/static/js/runtime-main.ec2927bf.js"
+  }
+]);
